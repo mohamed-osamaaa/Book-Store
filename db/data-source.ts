2da9c80a50635fd0835c93e1daab9b93143294fb
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-02-13 09:05:32 +0200

    commit2

---
+++ db/data-source.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+config();
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mariadb',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: 'typeorm',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+dataSource.initialize();
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { dataSourceOptions } from 'db/data-source';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
