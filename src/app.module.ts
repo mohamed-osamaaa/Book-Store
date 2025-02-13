f5a13fc2355df1394684dd891c181be2f629ac46
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-02-13 09:43:31 +0200

    commit4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { dataSourceOptions } from 'db/data-source';
 
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CurrentUserMiddleware } from './utility/middlewares/current-user.middleware';
 
 @Module({
   imports: [
@@ -17,4 +18,10 @@ import { UsersModule } from './users/users.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentUserMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
